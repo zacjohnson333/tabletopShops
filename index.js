98dabbb081aa17c4ad6aa4abec882b32e40a221a
Zac Johnson <zacjohnson333@gmail.com>
2022-09-21 10:51:20 -0500

    Create boilerplate file within new Layouts dir. Install ejs-   mate npm package.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Shop = require('./models/shop');
 
@@ -16,7 +17,7 @@ db.once("open", () => {
 
 const app = express();
 
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
