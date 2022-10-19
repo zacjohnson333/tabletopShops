35ce165f08677906b77442ec46954e081b8e989b
Zac Johnson <zacjohnson333@gmail.com>
2022-10-19 10:36:46 -0500

    Create Public dir (lead to in in index.js), stylesheets dir,   and style scss/css. Add basic margins boilerplate (temp?)

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));    // Can't serve statis files without this line, leads to the Public folder
 
 
 app.get('/', (req, res) => {
